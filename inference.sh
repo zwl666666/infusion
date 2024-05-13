6e37bd9172beb1b6c62db609713f94bbc66d25c8
zwl666666 <44702519+zwl666666@users.noreply.github.com>
2024-05-13 11:06:43 +0800

    Add files via upload

---
+++ inference.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+CUDA_VISIBLE_DEVICES=2 python scripts/perfusion_txt2img.py --ddim_eta 0.0 \
+                                    --outdir ./outputs/ori_cat_1500  \
+                                    --seed 2023 \
+                                    --steps 50  \
+                                    --scale 8.0 \
+                                    --beta 0.7 \
+                                    --tau 0.15 \
+                                    --n_samples 10 \
+                                    --n_iter 1 \
+                                    --personalized_ckpt ckpt/2023-12-16T17-47-12_cat/models/step=1500.ckpt \
+                                    --target_word "cat" \
+                                    --prompt "A cat is strolling through a bustling cityscape"
+
+
