19b40bc7972b73d5a855ff50467ebd1efb138491
zwl666666 <44702519+zwl666666@users.noreply.github.com>
2025-12-20 00:07:31 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,35 +1,7 @@
 #!/usr/bin/bash
 CUDA_VISIBLE_DEVICES=1 python main.py \
-    --name backpack_new \
-    --base ./configs/kv_train/backpack.yaml \
-    --basedir ./kv_ckpt \
-    -t True \
-    --gpus 0,
-
-CUDA_VISIBLE_DEVICES=1 python main.py \
-    --name cat_new \
-    --base ./configs/kv_train/cat.yaml \
-    --basedir ./kv_ckpt \
-    -t True \
-    --gpus 0,
-
-CUDA_VISIBLE_DEVICES=1 python main.py \
-    --name dog5_new \
-    --base ./configs/dog5.yaml \
-    --basedir ./kv_ckpt \
-    -t True \
-    --gpus 0,
-
-CUDA_VISIBLE_DEVICES=1 python main.py \
-    --name pot_new \
-    --base ./configs/kv_train/pot.yaml \
-    --basedir ./kv_ckpt \
-    -t True \
-    --gpus 0,
-
-CUDA_VISIBLE_DEVICES=1 python main.py \
-    --name teddy_new \
-    --base ./configs/kv_train/teddy_bear.yaml \
+    --name cat \
+    --base ./configs/infusion_cat.yaml \
     --basedir ./kv_ckpt \
     -t True \
     --gpus 0,
